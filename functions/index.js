160cb868ad421a672babe6449c43afba2cd0a040
Elias Braz <elias.brazjunior@gmail.com>
2021-06-16 17:48:43 -0300

    Functions to send task notifications

--- functions/index.js
+++ functions/index.js
@@ -34,54 +34,125 @@ exports.newTask = functions.firestore
             }
         }
 
-        const notificationResult = await admin
-            .messaging()
-            .sendToDevice(tokens, notification)
-
-        notificationResult.results.forEach(r => {
-            if (r.error) {
-                functions.logger.error(`An ocurred error. taskId: ${snap.id}`, r.error.message)
-            }
-        })
+        await sendNotification(tokens, notification, snap);
     });
 
-exports.updateUser = functions.firestore
-    .document('users/{userId}')
-    .onUpdate(async (change, context) => {
-        const id = change.after.id
-        const previousValue = change.before.data()
-        const newValue = change.after.data()
+// exports.updateUser = functions.firestore
+//     .document('users/{userId}')
+//     .onUpdate(async (change, context) => {
+//         const id = change.after.id
+//         const previousValue = change.before.data()
+//         const newValue = change.after.data()
 
-        for await (const minister of newValue.ministers) {
-            const previousMinisters = previousValue.ministers.map(m => m.id)
-            if (!previousMinisters.includes(minister.id)) {
-                admin
-                    .firestore()
-                    .collection('ministers')
-                    .doc(minister.id)
-                    .get()
-                    .then(data => {
-                        let m = data.data()
-                        if (m.users) {
-                            m.users.push(id)
-                        } else {
-                            m.users = [id]
-                        }
+//         functions.logger.info(`-- news ministers: ${newValue.ministers}`)
+//         for await (const minister of newValue.ministers) {
+//             const previousMinisters = previousValue.ministers.map(m => m.id)
+//             if (!previousMinisters.includes(minister.id)) {
+//                 admin
+//                     .firestore()
+//                     .collection('ministers')
+//                     .doc(minister.id)
+//                     .get()
+//                     .then(data => {
+//                         let m = data.data()
+//                         if (m.users) {
+//                             m.users.push(id)
+//                         } else {
+//                             m.users = [id]
+//                         }
 
-                        admin
-                            .firestore()
-                            .collection(ministers)
-                            .doc(minister.id)
-                            .set({ users: m.users })
-                            .catch(error => {
-                                functions.logger.error(`An ocorred error while update minister: ${minister.id}`, error)
-                            })
-                    })
-                    .catch(error => {
-                        functions.logger.error(`An ocorred error while find minister: ${minister.id}`, error)
-                    })
+//                         admin
+//                             .firestore()
+//                             .collection(ministers)
+//                             .doc(minister.id)
+//                             .set({ users: m.users })
+//                             .catch(error => {
+//                                 functions.logger.error(`An ocorred error while update minister: ${minister.id}`, error)
+//                             })
+//                     })
+//                     .catch(error => {
+//                         functions.logger.error(`An ocorred error while find minister: ${minister.id}`, error)
+//                     })
+//             }
+//         }
+
+
+//     })
+
+exports.sendDailyNotifications = functions.pubsub
+    .schedule('every 08:00')
+    .onRun(context => {
+        let todayStart = moment()
+        let todayEnd = moment()
+        todayStart.startOf('day')
+        todayEnd.endOf('day')
+
+        const todayTasks = await admin.firestore()
+            .collection('tasks')
+            .where('date', '>', todayStart.toDate())
+            .where('date', '<', todayEnd.toDate())
+            .get()
+
+        for await (const doc of todayTasks.docs.map(d => ({ id: d.id, ...d.data() }))) {
+            let ministry = await admin.firestore()
+                .collection('users')
+                .doc(doc.ministry.id)
+                .get()
+            const tokens = ministry.data().tokens || []
+
+            const notification = {
+                notification: {
+                    title: 'Não se esquece hein!',
+                    body: `Hoje você está escalado no ministério: ${doc.minister.name}`
+                }
             }
+
+            await sendNotification(tokens, notification, doc.id)
         }
-
-
     })
+
+exports.sendTomorrowNotifications = functions.pubsub
+    .schedule('every 08:00')
+    .onRun(context => {
+        let tomorrowStart = moment()
+        let tomorrowEnd = moment()
+        tomorrowStart.add(1, 'day')
+        tomorrowEnd.add(1, 'day')
+        tomorrowStart.startOf('day')
+        tomorrowEnd.endOf('day')
+
+        const todayTasks = await admin.firestore()
+            .collection('tasks')
+            .where('date', '>', tomorrowStart.toDate())
+            .where('date', '<', tomorrowEnd.toDate())
+            .get()
+
+        for await (const doc of todayTasks.docs.map(d => ({ id: d.id, ...d.data() }))) {
+            let ministry = await admin.firestore()
+                .collection('users')
+                .doc(doc.ministry.id)
+                .get()
+            const tokens = ministry.data().tokens || []
+
+            const notification = {
+                notification: {
+                    title: 'Não se esquece hein!',
+                    body: `Amanhã você está escalado no ministério: ${doc.minister.name}`
+                }
+            }
+
+            await sendNotification(tokens, notification, doc.id)
+        }
+    })
+
+async function sendNotification(tokens, notification, taskId) {
+    const notificationResult = await admin
+        .messaging()
+        .sendToDevice(tokens, notification);
+
+    notificationResult.results.forEach(r => {
+        if (r.error) {
+            functions.logger.error(`An ocurred error. taskId: ${taskId}`, r.error.message);
+        }
+    });
+}
